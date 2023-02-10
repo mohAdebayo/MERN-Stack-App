2dff150845f1633c4ea7810489f64f2eb35f61e1
Victor <vsifah@gmail.com>
2023-02-10 12:07:53 +0100

    editing the app.js file

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const JWTSECRET = process.env.JWTSECRET;
 
 module.exports = {
     jwtSecret: JWTSECRET,
-    mongodburi: 'mongodb://' + 'username' + ':' + 'password' + '@mongodb:27017/basic-mern-stack-app'
+    mongodburi: 'mongodb://username:password@mongodb:27017/basic-mern-stack-app'
 };
