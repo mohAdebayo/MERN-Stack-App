f8a0a87236310deceb2ea5894e2ec7c9f0cf4ed7
Victor <vsifah@gmail.com>
2023-03-18 21:26:44 +0100

    editz

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const JWTSECRET = process.env.JWTSECRET;
 
 module.exports = {
     jwtSecret: JWTSECRET,
-    mongodburi: `mongodb://admin:password@mongodb:27017/basic-mern-app`
+    mongodburi: `mongodb://username:password@mongodb:27017/basic-mern-app`
 };
